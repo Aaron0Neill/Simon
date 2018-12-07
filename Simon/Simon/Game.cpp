fc82a5d47cd63446be0ccf31f17a54033083a3dc
(Student) - Aaron O Neill <C00241596@ITCARLOW.IE>
2018-12-07 10:09:20 +0000

    Added all the buttons

--- Simon/Simon/Game.cpp
+++ Simon/Simon/Game.cpp
@@ -8,7 +8,10 @@
 Game::Game() :
 	m_window{ sf::VideoMode{ 800, 600, 32 }, "Simon" },
 	m_exitGame{ false }, //when true game will exit
-	m_redSquare{ sf::Vector2f{ 200.0f, 200.0f } }
+	m_redSquare{ sf::Vector2f{ 200.0f, 200.0f } },
+	m_yellowSquare{sf::Vector2f{ 200.0f, 200.0f } },
+	m_greenSquare{ sf::Vector2f{200.0f, 200.0f} },
+	m_blueSquare{sf::Vector2f{ 200.0f, 200.0f } }
 {
 	setupButton();
 }
@@ -80,11 +83,25 @@ void Game::render()
 {
 	m_window.clear();
 	m_window.draw(m_redSquare);
+	m_window.draw(m_yellowSquare);
+	m_window.draw(m_greenSquare);
+	m_window.draw(m_blueSquare);
 	m_window.display();
 }
-
+/// <summary>
+/// Sets up all the buttons
+/// </summary>
 void Game::setupButton()
 {
-	m_redSquare.setFillColor(sf::Color::Red);
+	m_redSquare.setFillColor(RED);
 	m_redSquare.setPosition(sf::Vector2f{ 570, 30 });
+
+	m_greenSquare.setFillColor(GREEN);
+	m_greenSquare.setPosition(sf::Vector2f(350, 30));
+
+	m_yellowSquare.setFillColor(YELLOW);
+	m_yellowSquare.setPosition(sf::Vector2f(350, 250));
+
+	m_blueSquare.setFillColor(BLUE);
+	m_blueSquare.setPosition(sf::Vector2f(570, 250));
 }
--- Simon/Simon/Game.h
+++ Simon/Simon/Game.h
@@ -21,13 +21,23 @@ private:
 	void render();
 	void setupButton();
 
+	const sf::Color RED{ 255, 30, 30, 255 }; // it's the color red
+	const sf::Color GREEN{ 51, 255, 51, 255 }; // it's the color green
+	const sf::Color BLUE{ 0, 255, 255, 255 }; // it's the color blue
+	const sf::Color YELLOW{ 255, 255, 0, 255 }; // it's the color yellow
+	const sf::Color WHITE{ 255, 255, 255, 255 }; // it's the color white
+
+
 	sf::RenderWindow m_window; // main SFML window
 	sf::Font m_ArialBlackfont; // font used by message
 	sf::Text m_welcomeMessage; // text used for message on screen
 	sf::Texture m_logoTexture; // texture used for sfml logo
 	sf::Sprite m_logoSprite; // sprite used for sfml logo
 	bool m_exitGame; // control exiting game
-	sf::RectangleShape m_redSquare;
+	sf::RectangleShape m_redSquare; // it's rectangle shape for a red button.
+	sf::RectangleShape m_yellowSquare; // it's rectangle shape for a yellow button.
+	sf::RectangleShape m_blueSquare; // it's rectangle shape for a blue button.
+	sf::RectangleShape m_greenSquare; // it's rectangle shape for a green button.
 
 };
 
